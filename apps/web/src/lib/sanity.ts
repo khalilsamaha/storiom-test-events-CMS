a59a3ed6508bfd1d38abe0e6dd08b411e55414d5
Khalil Samaha <khalil.samaha@hotmail.com>
2026-06-20 09:48:14 +0200

    Fix Sanity Studio schema and client wrapper

--- apps/web/src/lib/sanity.ts
+++ apps/web/src/lib/sanity.ts
@@ -2,8 +2,8 @@ import {createClient} from "next-sanity"
 import imageUrlBuilder from "@sanity/image-url"
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "f5m1d5ui",
+dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2026-06-18",
   useCdn: true,
 })
